1c955a97b3ee39f8de2e9cb4a7564b6b300440c6
markeilander <mark.eilander@outlook.com>
2016-04-05 15:34:39 +0200

    Added hexadecimal
    Added hexadecimal

--- src/ValidatorExtended.php
+++ src/ValidatorExtended.php
@@ -8,6 +8,7 @@ class ValidatorExtended extends IlluminateValidator {
 
 	protected $messages = array(
 		"extended_alpha_num" => "The :attribute may only contain letters, spaces, and dashes.",
+		"hexadecimal" => "The :attribute must follow format #[a-fA-F0-9]",
 	);
 
 	public function __construct( $translator, $data, $rules, $messages = array(), $customAttributes = array() ) {
@@ -23,7 +24,16 @@ class ValidatorExtended extends IlluminateValidator {
 	 * @return bool
 	 */
 	protected function validateExtendedAlphaNum( $attribute, $value) {
-
 		return preg_match("/^[\pL\pM\pN\s_\-?!\"#$%&'@.,:;]+$/u", $value);
+	}
+	/**
+	 * Hexadecimal
+	 *
+	 * @param string $attribute
+	 * @param mixed $value
+	 * @return bool
+	 */
+	protected function validateHexadecimal( $attribute, $value) {
+		preg_match('^#[a-fA-F0-9]{6}$/i', $value);
 	}
 }	//end of class
